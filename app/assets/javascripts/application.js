fcdfb88e0bd434cf2df8f6e6bb74671f575b4c96
Your Name <you@example.com>
2019-09-26 23:51:40 +0900

    settings

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,12 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+
+//= require jquery_ujs
 //= require_tree .
 //= require jquery
+
 //= require turbolinks
 //= require popper
 //= require bootstrap-sprockets
