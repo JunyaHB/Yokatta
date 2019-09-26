9064d4f5db9128d5e9955b35a7134bc30509017c
Your Name <you@example.com>
2019-09-26 22:52:43 +0900

    changesettings

--- db/migrate/20190924100521_change_likes_user_id_comment_id_not_null.rb
+++
@@ -1,8 +0,0 @@
-class ChangeLikesUserIdCommentIdNotNull < ActiveRecord::Migration[5.2]
-  def change
-    change_column_null :likes, :user_id, false
-    change_column_null :likes, :comment_id, false
-  end
-end
-
-
