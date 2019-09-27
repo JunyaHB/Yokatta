e8276cb67e94a7affb509f73bd3f39e369194f16
Your Name <you@example.com>
2019-09-27 18:46:11 +0900

    Addhttps

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
 
   def index
     @posts = Post.all.order(created_at: :desc)
-    @posts = Post.page(params[:page]).per(12)
+    @posts = Post.page(params[:page]).per(12).order('created_at DESC')
   end
 
   def edit
