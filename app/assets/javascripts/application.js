6109bed565941b5dc783ff7a1431e76ef83f1dc8
Your Name <you@example.com>
2019-09-27 17:45:46 +0900

    Addgrid

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 //
 //= require jquery
 //= require_tree .
+//= require turbolinks
 //= require rails-ujs
 //= require bootstrap
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
-
   validates :content, {presence: true, length: {maximum: 84}}  
-  validates :category, {presence: true}  
-
 end
