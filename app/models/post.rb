e289cd017436a85a96d4cc3545ca3bcd13ab2aca
Your Name <you@example.com>
2019-09-27 17:59:23 +0900

    setteings

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :user
-  validates :content, {presence: true, length: {maximum: 84}}  
+  validates :content, {presence: true, length: {maximum: 150}}  
 end
