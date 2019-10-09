d32af934fd8f1db9db23634b2880e6b711e05f09
Your Name <you@example.com>
2019-10-09 20:01:29 +0900

    AddEmailValidate

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_secure_password
   has_many :posts
 
   mount_uploader :image_name, ImageNameUploader
   validates :name, {presence: true, uniqueness: true, length: {maximum: 10}}  
-  validates :email, {presence: true, uniqueness: true}  
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, {presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true}  
 
 end
